21851931712efb929a5b23dd9054b9812135431f
loubenjin <408042463@qq.com>
2016-12-20 11:44:40 +0800

    6

--- modules/zhuye/js/main1.js
+++ modules/zhuye/js/main1.js
@@ -28,18 +28,6 @@ define(["text!../zhuye.html","css!../css/main.css"], function(homePage){
 				$(".here").css({"display":"inline-block"});
 				$(".here").text(n++);
 			});*/
-			var htmlstr= baidu.template("firstTmp",{zhuye});
-			$('#swiper-wrapper').html(htmlstr);
-			$(this).find("#firstTmp").remove();
-			var htmlstr1= baidu.template("secondTmp",{zhuye});
-			$('.four-ul').html(htmlstr1);
-			$(this).find("#secondTmp").remove();
-			var htmlstr2= baidu.template("thrTmp",{zhuye});
-			$('.three').html(htmlstr2);
-			$(this).find("#thrTmp").remove();
-			var htmlstr3= baidu.template("fourTmp",{zhuye});
-			$('.bigul').html(htmlstr3);
-			$(this).find("#fourTmp").remove();			
 			var mySwiper = new Swiper('.swiper-container', {
 				autoplay: 2000,//可选选项，自动滑动(时间:毫秒)
 				autoplayDisableOnInteraction : false,
@@ -56,5 +44,17 @@ $.ajax({
 		data:"name=tom&age=22",
 		success:function(responseData){
 		zhuye=responseData.data.act_info;
+			var htmlstr= baidu.template("firstTmp",{zhuye});
+			$('#swiper-wrapper').html(htmlstr);
+			$(this).find("#firstTmp").remove();
+			var htmlstr1= baidu.template("secondTmp",{zhuye});
+			$('.four-ul').html(htmlstr1);
+			$(this).find("#secondTmp").remove();
+			var htmlstr2= baidu.template("thrTmp",{zhuye});
+			$('.three').html(htmlstr2);
+			$(this).find("#thrTmp").remove();
+			var htmlstr3= baidu.template("fourTmp",{zhuye});
+			$('.bigul').html(htmlstr3);
+			$(this).find("#fourTmp").remove();			
 		}
 });
